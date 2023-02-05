010829a51d04ca228b6d687c41cccb98d0720297
bartek <bm417744@students.mimuw.edu.pl>
2023-02-05 20:44:36 +0100

    update paths, add reading files

--- func.py
+++ func.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import os
 
 cwd=os.getcwd()
@@ -8,8 +9,18 @@ file_SP=os.path.join(cwd, os.path.normpath('API_SP.POP.TOTL_DS2_en_csv_v2_475160
 file_co2=os.path.join(cwd, os.path.normpath('co2-fossil-by-nation_zip/data/fossil-fuel-co2-emissions-by-nation_csv.csv'))
 
 gdp = pd.read_csv(file_NY, skiprows=3)
-populacja = pd.read_csv(file_SP, skiprows=3)
-co2 = pd.read_csv(file_co2, skiprows=0)
+population = pd.read_csv(file_SP, skiprows=3)
+co2 = pd.read_csv(file_co2)
+
+def int_if_possible(input_str):
+    try:
+        return int(input_str)
+    except ValueError:
+        return None
+
+gdp.dropna(how='all', axis=1, inplace=True)
+population.dropna(how='all', axis=1, inplace=True)
+common_years = set(np.unique(co2.Year)) & set(map(int_if_possible, gdp.columns)) & set(map(int_if_possible, population.columns))
 
 
 

adc434ada346cf4c7d1e057460969ecedb0d3025
bartek <bm417744@students.mimuw.edu.pl>
2023-02-05 22:42:07 +0100

    small formatting change

--- func.py
+++ func.py
@@ -8,8 +8,8 @@ file_NY = os.path.join(cwd, os.path.normpath(
     'API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562/API_NY.GDP.MKTP.CD_DS2_en_csv_v2_4751562.csv'))
 file_SP = os.path.join(cwd, os.path.normpath(
     'API_SP.POP.TOTL_DS2_en_csv_v2_4751604/API_SP.POP.TOTL_DS2_en_csv_v2_4751604.csv'))
-file_co2 = os.path.join(cwd,
-                        os.path.normpath('co2-fossil-by-nation_zip/data/fossil-fuel-co2-emissions-by-nation_csv.csv'))
+file_co2 = os.path.join(cwd, os.path.normpath(
+    'co2-fossil-by-nation_zip/data/fossil-fuel-co2-emissions-by-nation_csv.csv'))
 
 gdp = pd.read_csv(file_NY, skiprows=4)
 population = pd.read_csv(file_SP, skiprows=4)

cdd5d7e74cbe764f14f1cecacc1b3b257d6b774e
bartek <bm417744@students.mimuw.edu.pl>
2023-02-05 21:30:36 +0100

    better common names

--- func.py
+++ func.py
@@ -8,8 +8,8 @@ file_NY=os.path.join(cwd, os.path.normpath('API_NY.GDP.MKTP.CD_DS2_en_csv_v2_475
 file_SP=os.path.join(cwd, os.path.normpath('API_SP.POP.TOTL_DS2_en_csv_v2_4751604/API_SP.POP.TOTL_DS2_en_csv_v2_4751604.csv'))
 file_co2=os.path.join(cwd, os.path.normpath('co2-fossil-by-nation_zip/data/fossil-fuel-co2-emissions-by-nation_csv.csv'))
 
-gdp = pd.read_csv(file_NY, skiprows=3)
-population = pd.read_csv(file_SP, skiprows=3)
+gdp = pd.read_csv(file_NY, skiprows=4)
+population = pd.read_csv(file_SP, skiprows=4)
 co2 = pd.read_csv(file_co2)
 
 def int_if_possible(input_str):
@@ -20,9 +20,10 @@ def int_if_possible(input_str):
 
 gdp.dropna(how='all', axis=1, inplace=True)
 population.dropna(how='all', axis=1, inplace=True)
-
-common_years = set(np.unique(co2.Year)) & set(map(int_if_possible, gdp.columns)) & set(map(int_if_possible, population.columns))
-
-
+non_year_colnames = ['Country Name', 'Country Code', 'Indicator Name', 'Indicator Code']
+common_years = set.intersection(set(np.unique(co2.Year)), set(map(int_if_possible, gdp.columns)), set(map(int_if_possible, population.columns)))
+#common_years = list(map(str, common_years))
+print(gdp[non_year_colnames + list(map(str, common_years))])
+print(population[non_year_colnames + list(map(str, common_years))])
 
 

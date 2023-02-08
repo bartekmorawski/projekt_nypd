ddff354c1841a0945427ac73f35223b2608f8a58
bartek <bm417744@students.mimuw.edu.pl>
2023-02-08 17:41:26 +0100

    change setup

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     version='1.0',
     license='BSD 2-clause',
     author="bartek morawski",
-    packages=setuptools.find_packages(where="./src"),
+    package_dir={"":"src"},
+    packages=setuptools.find_packages(),
     install_requires=['pandas', 'numpy', 'argparse', 'openpyxl']
 )
